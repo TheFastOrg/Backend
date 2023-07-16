4d073155d489ed14383fc38dce9f9de03a5ae9bf
bayoog <obaydaba96@gmail.com>
2023-07-16 14:09:36 +0200

    feat: add drf-social-oauth

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,16 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("ba7besh.urls")),
-    path(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    # path(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    re_path(r"^auth/", include("drf_social_oauth2.urls", namespace="drf")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
     # Optional UI:
-    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-
+    path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
